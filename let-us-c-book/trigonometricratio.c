b1e64a657ec59a000eef78272526bac51b67f8b2
shishupal <shishupal@shishupals-MacBook-Pro.local>
2021-03-31 23:54:03 +0530

    trigonometricratio.c

--- let-us-c-book/trigonometricratio.c
+++ let-us-c-book/trigonometricratio.c
@@ -1,1 +1,16 @@
-#include<
+#include<stdio.h>
+#include <math.h>
+int main()
+{
+	float s,c,t,angle;
+	printf("Enter an angle\n");
+	scanf ("%f",&angle);
+	//convert angle to radians
+	angle=angle*3.14/180;
+	s=sin(angle);
+	c=cos(angle);
+	t=tan(angle);
+	printf("sin=%f\n cos=%f\n tan=%f\n",s,c,t );
+    return 0;
+
+}
