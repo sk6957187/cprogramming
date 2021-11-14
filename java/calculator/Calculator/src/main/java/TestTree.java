4a49e743b92fa11f2c540a5b681cf4b6d504c185
shishupal <shishupal@shishupals-MacBook-Pro.local>
2021-11-14 09:33:10 +0530

    added java code

---
+++ java/calculator/Calculator/src/main/java/TestTree.java
@@ -0,0 +1,66 @@
+//import src.Tree;
+
+import src.Stack;
+import src.Tree;
+
+import java.util.ArrayList;
+import java.lang.String;
+
+public class TestTree {
+    private Tree tempTree = new Tree();
+    private int getResult(ArrayList<String> posixArray) {
+        int result;
+        int a,b,c = 0, i;
+        int temp;
+        Stack stack = new Stack();
+        for(i=0; i<posixArray.size(); i++) {
+            if(!tempTree.isSymbol(posixArray.get(i))) {
+                stack.push(Integer.parseInt(posixArray.get(i)));
+                continue;
+            }
+            a = stack.pop();
+            b = stack.pop();
+            if(posixArray.get(i).equals("+")) {
+                c = a+b;
+            }
+            if(posixArray.get(i).equals("-")) {
+                c = b-a;
+            }
+            if(posixArray.get(i).equals("*")) {
+                c = a*b;
+            }
+            if(posixArray.get(i).equals("/")) {
+                c = b/a;
+            }
+            stack.push(c);
+        }
+        result = stack.pop();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TestTree testTree = new TestTree();
+        String input = "12 -1*2-2+30/5";
+        ArrayList<String> arrOfinput = Stack.tokenizeStringNumeric(input);
+        System.out.println(arrOfinput);
+        Tree root = null;
+//        for (int i=0; i<input.length(); i++) {
+//            Tree treeNode = Tree.createNode();
+//            treeNode.data = input.substring(i, i+1);
+//            root = treeNode.insertNode(root, treeNode, treeNode.data);
+//        }
+        for (int i=0; i<arrOfinput.size(); i++) {
+            Tree treeNode = Tree.createNode();
+            treeNode.data = arrOfinput.get(i);
+            root = treeNode.insertNode(root, treeNode, treeNode.data);
+        }
+        ArrayList<String> arr = new ArrayList<String>();
+        testTree.tempTree.convertTreeToPosix(arr, root);
+        int result = testTree.getResult(arr);
+        //String[] str = testTree.tempTree.split("+");
+      //String[] arrOfstr = str.split("+");
+        System.out.println(arr);
+        System.out.println(result);
+        //System.out.println(str);
+    }
+}
