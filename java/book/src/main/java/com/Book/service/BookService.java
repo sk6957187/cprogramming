be1a455090f993ce9e1cf80bf5fc554504b64bb0
shishupal <shishupal@shishupals-MacBook-Pro.local>
2022-02-12 15:25:09 +0530

    adding to all file in book

--- java/book/src/main/java/com/Book/service/BookService.java
+++ java/book/src/main/java/com/Book/service/BookService.java
@@ -13,30 +13,59 @@ public class BookService {
         public BookService() {
             super();
 
-            if(BookIdMap==null)
-            {
-                BookIdMap=new HashMap<String, Book>();
-                // Creating some objects of Book while initializing
-                Book javaBook=new Book("1", "Head first java","400");
-                Book springBook=new Book("4", "Spring in action","500");
-                Book pythonBook=new Book("3", "Learning Python","250");
-                Book hiberanteBook=new Book("2", "Hibernate in action","300");
+        }
+        private Book getBookFromCsvData(int bookId, ArrayList<String> csvBook) {
+            if (csvBook == null) {
+                return null;
+            }
+            String id, title, pages;
+            Book tempBook = null;
+            if (csvBook.size() >= 3) {
+                id = Integer.toString(bookId);//csvBook.get(0);
+                title = csvBook.get(1);
+                pages = csvBook.get(2);
+                tempBook = new Book(id, title, pages);
+            }
+            return tempBook;
+        }
+        private void generateBookIdMap() {
+            ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
+            CSVFileService CSVFileService = new CSVFileService();
+            arr = CSVFileService.readFile("meta-data/book-record.csv", 0);
+            System.out.println(arr);
+            BookIdMap = new HashMap<String, Book>();
+            // Creating some objects of Book while initializing
+//                Book javaBook=new Book("1", "Head first java","400");
+//                Book springBook=new Book("4", "Spring in action","500");
+//                Book pythonBook=new Book("3", "Learning Python","250");
+//                Book hiberanteBook=new Book("2", "Hibernate in action","300");
+//
+//                BookIdMap.put("1", javaBook);
+//                BookIdMap.put("4", springBook);
+//                BookIdMap.put("3", pythonBook);
+//                BookIdMap.put("2", hiberanteBook);
+            Book tempBook;
+            int bookId = 1;
+            if (arr != null) {
+                for (int i=0; i<arr.size(); i++) {
+                    tempBook = this.getBookFromCsvData(bookId, arr.get(i));
+                    if (tempBook != null) {
+                        bookId++;
+                        BookIdMap.put(tempBook.getId(), tempBook);
+                    }
 
-                BookIdMap.put("1", javaBook);
-                BookIdMap.put("4", springBook);
-                BookIdMap.put("3", pythonBook);
-                BookIdMap.put("2", hiberanteBook);
+                }
             }
         }
-
         public List<Book> getAllBooks()
         {
+            this.generateBookIdMap();
             List<Book> books = new ArrayList<>(BookIdMap.values());
             return books;
         }
 
-        public Book getBook(String id)
-        {
+        public Book getBook(String id) {
+            this.generateBookIdMap();
             Book Book= BookIdMap.get(id);
             return Book;
         }
---
+++ java/book/src/main/java/com/Book/service/CSVFileService.java
@@ -0,0 +1,46 @@
+package com.Book.service;
+
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CSVFileService  {
+    public ArrayList<ArrayList<String>> readFile(String pathname, int index) {
+        ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
+        ArrayList<String> tempArr = new ArrayList<String>();
+        try {
+            // Create an object of file reader
+            // class with CSV file as a parameter.
+            FileReader filereader = new FileReader(pathname);
+
+            // create csvReader object and skip first Line
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withSkipLines(0)
+                    .build();
+            List<String[]> allData = csvReader.readAll();
+
+            // print Data
+            for (String[] row : allData) {
+                for (String cell : row) {
+                    tempArr.add(cell);
+                }
+                arr.add(tempArr);
+                tempArr = new ArrayList<String>();
+            }
+            /*for (String[] row : allData) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return arr;
+    }
+}
