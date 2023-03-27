eb9bd7eaa5d63b1e535492dfd92040680977fd82
Sumit Kumar <sk6957187@gmail.com>
2023-03-27 23:01:37 +0530

    added c-program code

---
+++ c-program/fibonacci_series.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void fib(int n)
+{
+	static int n1=0,n2=1,n3;
+	//n1=0;
+	//n2=1;
+	if(n>0) {
+		n3=n1+n2;
+		n1=n2;
+		n2=n3;
+		printf("%d ",n3);
+		fib(n-1);
+	}
+
+}
+int main()
+{
+	int n;
+	printf("Enter the number: ");
+	scanf("%d",&n);
+	printf("Fibbonacci series is :");
+	printf(" 0 1 ");
+	fib(n);
+	printf("\n");
+
+}
---
+++ c-program/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
