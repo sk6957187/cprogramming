b1bbc0d2caa1aa6d75b4efa7923b8beac7b6dab0
Sumit Kumar <sk6957187@gmail.com>
2023-03-28 00:09:17 +0530

    added code

---
+++ java/DropwizardHelloWordExample/src/main/java/org/arpit/java2blog/model/Book.java
@@ -0,0 +1,37 @@
+package org.arpit.java2blog.model;
+
+public class Book {
+
+    int id;
+    String title;
+    long pages;
+
+    public Book()
+    {
+
+    }
+    public Book(int id, String title, long pages) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.pages = pages;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public long getPages() {
+        return pages;
+    }
+    public void setPages(long pages) {
+        this.pages = pages;
+    }
+}
