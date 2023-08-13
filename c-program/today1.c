eb3de5741ee33fe25e15611a040c0df997ab923d
Sumit Kumar <sk6957187@gmail.com>
2023-08-14 01:34:32 +0530

    update

--- c-program/today1.c
+++ c-program/today1.c
@@ -1,52 +1,123 @@
-#include<stdio.h>  
-#include <math.h>
-#include <conio.h>
- main()
+ #include <stdio.h>
+ #include<stdlib.h>
+
+// struct Node{
+// 	int data;
+// 	struct Node * Next;
+// };
+
+// void linkedListTraversal(struct Node* ptr){
+// 	while(ptr != NULL) {
+// 		printf("Element = %d\n",ptr->data);
+// 		ptr = ptr->Next;
+// 	}
+// }
+
+// int main()
+// {
+// 	int n,a;
+// 	struct Node *head;
+// 	struct Node *second;
+// 	struct Node *third;
+// 	struct Node *fourth;
+
+// 	head = (struct Node*)malloc(sizeof(struct Node));
+// 	second = (struct Node*)malloc(sizeof(struct Node));
+// 	third = (struct Node*)malloc(sizeof(struct Node));
+// 	fourth = (struct Node*)malloc(sizeof(struct Node));
+
+// 	head -> data=1;
+// 	head->Next=second;
+// 	second -> data=2;
+// 	second->Next=third;
+// 	third -> data=3;
+// 	third->Next=fourth;
+// 	fourth -> data=4;
+// 	fourth->Next=NULL;
+
+// 	linkedListTraversal(head);
+
+// 	return 0;
+// }
+
+
+struct node{
+	int data;
+	struct node * next;
+};
+
+struct node *start = NULL;
+
+struct node* createnode()
 {
-	int k, n, r,l,m, Factorial;
-	printf("Enter the number\n");
-	scanf("%d%d",&n,&r);
-	k=factorialofN(n);
-    l=factorialofR(r);
-	m=factorialofnr(n,r);
-	Factorial = k/(l*m);
-	printf("Factorial of %d and %d is: %d\n", n, r,Factorial );
-  return;
- }
+	struct node *n;
+	n=(struct node *)malloc(sizeof(struct node));
+	n->next=NULL;
+	return(n);
+}
 
-  int factorialofN(inta)
-  {
-  	int a, factorial;
-   for(a=1; a<=inta; a++)
-   {
-   	factorial = factorial * a;
-   	return (factorial);
-   }
-  }
-  int factorialofR(intb)
+void viewlist()
 {
-  	int b,factorial;
-    for(b=1; b<=intb; b++)
-    {
-    	factorial= factorial*b;
-    	return(factorial);
-    }
-  }
+	struct node *temp;
+	if(start==NULL){
+		printf("List is empty\n");
+	}
+	else{
+		temp=start;
+		while(temp != NULL){
+			printf("%d",temp->data);
+			temp=temp->next;
+		}
+	}
+	printf("\n");
+}
 
-  int factorialofnr(intc, intd) 
-  {
-  	int Z, factorial;
-  	for( Z=1; Z<=(intc-intd); Z++)
-  		factorial=factorial*Z;
-  	return(factorial);
-  }
+void insertnode()
+{
+	struct node *temp,*t;
+	temp = createnode();
+	printf("Enter the number --");
+	scanf("%d",&temp->data);
+	if (start==NULL){
+		start=temp;
+	}
+	else{
+		t=start;
+		while(t !=NULL){
+			t=t->next;
+		}
+		printf("\n1__\t");
+		t->next=temp;
+		printf("2\t");
+	}
+	printf("Final list:-\n");
+	viewlist();
+}
 
+int prog(){
+	int ch;
+    printf("*******\n");
+    printf("1 Add value to the list\n");
+    printf("2 delete first value\n");
+    printf("3 View list\n");
+    printf("4 Exit\n");
+    printf("Enter your choice:  ");
+    scanf("%d",&ch);
+    //prog(ch);
+    return(ch);
+}
 
-
-
-
-
-
-
-
-
+int main()
+{
+	while(1)
+		switch(prog()){
+			case 1:
+                insertnode();
+                break;
+	        case 4:
+	                exit(0);
+	         default:
+	                printf("Invalid choice\n");
+		}
+	
+}
