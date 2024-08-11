5a31f61f12c9adc8378608e5dde3a9a9a898db22
Sumit Kumar <sk6957187@gmail.com>
2024-08-11 23:10:31 +0530

    update gitignore

---
+++ SpringBoot/jounrallApp/jounrallApp/src/main/java/com/nayak/jounrallApp/JournalApplication.java
@@ -0,0 +1,14 @@
+package com.nayak.jounrallApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
