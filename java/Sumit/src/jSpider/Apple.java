f5c71d22352c81f9f3a7046d68f54fc1b89d033e
Sumit Kumar <sk6957187@gmail.com>
2024-08-25 20:29:53 +0530

    added new

---
+++ java/Sumit/src/jSpider/Apple.java
@@ -0,0 +1,10 @@
+package jSpider;
+
+public class Apple {
+	public static void main(String[] args) {
+		float aP=325f;
+		float w=843f;
+		float t =  aP*w/1000.0f;
+		System.out.println("Price of apple: "+t);
+	}
+}
