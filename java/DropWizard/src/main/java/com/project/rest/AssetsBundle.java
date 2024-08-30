65b23409b3ad8cf001794ebc177aca48eda61ea2
Sumit Kumar <sk6957187@gmail.com>
2024-08-30 22:19:51 +0530

    remove not required files

--- java/DropWizard/src/main/java/com/project/rest/AssetsBundle.java
+++
@@ -1,7 +0,0 @@
-package com.project.rest;
-
-public class AssetsBundle {
-    public AssetsBundle(String s, String s1, String s2) {
-
-    }
-}
--- java/DropWizard/src/main/java/com/project/rest/AssetsBundleConfiguration.java
+++
@@ -1,4 +0,0 @@
-package com.project.rest;
-
-public class AssetsBundleConfiguration {
-}
