56aa78b5797642c51f163e9d269cc57ff187faf0
Sumit Kumar <sk6957187@gmail.com>
2024-09-16 22:15:48 +0530

    added test db sql

---
+++ mysql/testdb.sql
@@ -0,0 +1,9 @@
+create table emp(
+eid INTEGER,
+name varchar(20),
+post VARCHAR(20)
+);
+ INSERT INTO emp(eid,name,post) VALUES (1,'Sumit Kumar', 'SDE');
+ INSERT INTO emp(eid,name,post) VALUES (2,'Aman Kumar', 'SDE'); 
+  select * from emp;
+  
