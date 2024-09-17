d568bc43d8a13ec03dced8af90624d4a50a1b561
Sumit Kumar <sk6957187@gmail.com>
2024-09-17 09:40:25 +0530

    update

--- mysql/testdb.sql
+++ mysql/testdb.sql
@@ -5,5 +5,5 @@ post VARCHAR(20)
 );
  INSERT INTO emp(eid,name,post) VALUES (1,'Sumit Kumar', 'SDE');
  INSERT INTO emp(eid,name,post) VALUES (2,'Aman Kumar', 'SDE'); 
-  select * from emp;
+  select * from studentv2;
   
