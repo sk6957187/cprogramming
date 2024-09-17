7ee1475f09177780cd9ef2333e075c830055b759
Sumit Kumar <sk6957187@gmail.com>
2024-09-17 09:35:22 +0530

    update

--- reactjs/my-app/src/index.js
+++ reactjs/my-app/src/index.js
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
     <App/>
-    <BrowserRouter/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
