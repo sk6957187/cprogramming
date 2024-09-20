68022566029facdeeec046ae071613a7d0a22865
Sumit Kumar <sk6957187@gmail.com>
2024-09-20 21:52:33 +0530

    Add

---
+++ mysql/createTableInst_det.sql
@@ -0,0 +1,10 @@
+create table inst_det(i_id number(3) primary key, 
+    I_name varchar(15) not null, 
+    loc varchar(15) not null,
+    estd date, 
+    cont number(10) not null check(length(cont)=10));
+    
+insert into inst_det values(1, 'HIT', 'Haldia', '22-JUN-1996', 1334567890);
+insert into inst_det values(2, 'Jspider', 'BTM', '20-JAN-17', 1234567890);
+insert into inst_det values(3, 'JLC', 'BTM', '05-SEP-1999', 1234567898);
+insert into inst_det(i_id,I_name,loc,cont) values(4, 'Qspider', 'BTM', 1235675643);
