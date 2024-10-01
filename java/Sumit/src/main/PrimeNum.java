569c45a6b3844abee56fd4e0f32045fdee2ee811
Sumit Kumar <sk6957187@gmail.com>
2024-10-02 01:34:51 +0530

    Update'; git commit -m Update git commit -m "Update

--- java/Sumit/src/main/PrimeNum.java
+++ java/Sumit/src/main/PrimeNum.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 public class PrimeNum {
 	public static boolean isPrime(int n) {
-			 if(n <= 1 )
-				 return false;
-			 if(n==2)
-				 return true;
-			 if(n%2 == 0)
-				 return false;
-			 for (int i = 3; i <= Math.sqrt(n); i += 2) {
-		            if (n % i == 0)
-		                return false;
-		     }
-		     return true;
+		 if(n <= 1 ) {
+			 return false;
+		 }
+		 if(n==2) {
+			 return true;
+		 }
+		 if(n%2 == 0) {
+			 return false;
+		 }
+		 for (int i = 3; i <= Math.sqrt(n); i += 2) {
+	            if (n % i == 0) {
+	                return false;
+	            }
+	     }
+	     return true;
 		 
 		 
 	}
@@ -22,13 +26,13 @@ public class PrimeNum {
 	public static void main(String[] args) {
 	
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number");
+		System.out.print("Enter a number:- ");
 		int n = sc.nextInt();
-		if(isPrime(n))
+		if(isPrime(n)) {
 			System.out.println(n+" is a Prime number.");
-		else
+		}else {
 			System.out.println(n+" is not a Prime number.");
-	
+		}
 		sc.close();
 	}
 }
