9dba79c048dee66a5cdc3a1964c3e4fd51d98268
Sumit Kumar <sk6957187@gmail.com>
2026-06-15 22:13:47 +0530

    Update today file

--- java/Sumit/src/dsa/Today6.java
+++ java/Sumit/src/dsa/Today6.java
@@ -1,19 +1,39 @@
 package dsa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.Stack;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-public class Today6 {
+
+public class Today6{
+	
+		
 	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<>();
-		System.out.println("Stack is empty ->" + stack.isEmpty());
-		stack.push(10);
-		stack.push(20);
-		stack.push(30);
-		stack.push(40);
-		System.out.println("stack -> " +stack);
-		System.out.println("peek ->" +stack.peek());
-		System.out.println("Pop in stack ->"+ stack.pop());
-		System.out.println("stack -> " +stack);
-		System.out.println("Stack is empty ->"+stack.isEmpty());
+		List<Integer> list = new ArrayList<>(List.of(1,2,3,4));
+		int[] arr = {1,2,5,7,8};
+		list.add(10);
+		list.addAll(Arrays.stream(arr)
+				.boxed()
+				.toList());
+		
+		
+		System.out.println(list);
+		System.out.println();
+		
+		Integer sum = list.stream()
+						.mapToInt(Integer::intValue)
+						.sum();
+				
+		
 	}
 }
