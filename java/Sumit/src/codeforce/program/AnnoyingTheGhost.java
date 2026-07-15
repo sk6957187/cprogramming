2250ec0637fda80a44792ba306adcfb90a483f23
Sumit Kumar <sk6957187@gmail.com>
2026-07-15 22:57:35 +0530

    added codeforce code id is 2237B

---
+++ java/Sumit/src/codeforce/program/AnnoyingTheGhost.java
@@ -0,0 +1,19 @@
+package codeforce.program;
+
+import java.util.Scanner;
+
+//	Question id: 2237B
+
+public class AnnoyingTheGhost {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the no of test cases: ");
+		int t = sc.nextInt();
+		
+		while(t-- > 0) {
+			
+		}
+	}
+}
